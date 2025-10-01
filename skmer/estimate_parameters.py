9c11658aa0ef10a7646c6eb2eb509948a87c2fa4
Eduardo Charvel <echarvel3@Eduardos-MacBook-Pro-2.local>
2025-10-01 12:01:22 -0700

    refactoring reskmer code

--- skmer/estimate_parameters.py
+++ skmer/estimate_parameters.py
@@ -34,6 +34,23 @@ def count_kmers(sample_dir, sample, sequence, k, nth):
         histo_stderr = open(histo_file).read()
     return(histo_stderr)
 
+def sequence_stat(sequence):
+    total_length = 0
+    n_reads = 0
+    max_length = 0
+    # TODO: seqtk comp causing slowing issues in some clusters especially with Skmer subsample.
+    # NOTE: potential fix is to replace with "bbduk.sh in=X lhist=X.txt"
+    comp_stdout = check_output(["seqtk", "comp", sequence], stderr=STDOUT, universal_newlines=True)
+    reads_stat = comp_stdout.split('\n')
+    for stat in reads_stat:
+        if not stat.strip():
+            continue
+        read_length = sum([int(x) for x in stat.split('\t')[2:6]])
+        total_length += read_length
+        max_length = max(max_length, read_length)
+        n_reads += 1
+    return int(round(1.0 * total_length / n_reads)), max_length, total_length, n_reads
+
 def estimate_cov(sequence, lib, k, e, nth, ref_hist = None):
     sample = os.path.basename(sequence).rsplit('.f', 1)[0]
     sample_dir = os.path.join(lib, sample)
