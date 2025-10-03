29756e55937b8713f55d947543c68b244f471369
Eduardo Charvel <echarvel3@calab-fe.cluster>
2025-10-02 18:17:45 -0700

    separating coverage function

--- skmer/dipskmer/diploid_parameters.py
+++ skmer/dipskmer/diploid_parameters.py
@@ -24,11 +24,12 @@ def estimate_diploid_cov(sample, count, k, e, l, theta_arg):
         r =  count[ind + 1] / count[ind]
         rn =  count[ind + 2] / count[ind+1]
         if (8 * (2 + ind) * rn > 9 * (1 + ind) * r) or (theta_arg is not None):
-            sys.stderr.write('[WARNING] Not enough information to co-estimate coverage, theta, and error rate of {0}; '.format(sample) +
-                         'Using default theta but computing other values {0}\n'.format(default_theta))
             theta = default_theta if (theta_arg is None) else theta_arg
             Q = (1-theta)**k
             xi = minimize(lambda x: (r+x*(-2* np.exp(x)+2* (-2**ind+np.exp(x)) * Q)/((1+ind)* (2* np.exp(x)+(2**ind-2* np.exp(x))*Q )))**2, 0.5, bounds = [[0,100]]).x[0]
+            if theta_arg is None:
+                sys.stderr.write('[WARNING] Not enough information to co-estimate coverage, theta, and error rate of {0}; '.format(sample) +
+                         'Using default theta but computing other values {0}\n'.format(default_theta))
         else:
             xi = 1./4 * (3 * (1 + ind) * r - math.sqrt((1 + ind) * r) *math.sqrt(max(0, -8 * (2 + ind) * rn + 9 * (1 + ind) * r)))
             theta = 1 - 2**(1/ k) * ((np.exp(xi) * (-r - ind* r + xi))/( 2**ind *r - 2 * np.exp(xi) * r + 2**ind * ind * r - 2* np.exp(xi) * ind* r - 2**(1 + ind) * xi + 2 * np.exp(xi)* xi))**(1/k) 
--- skmer/estimate_parameters.py
+++ skmer/estimate_parameters.py
@@ -55,7 +55,7 @@ def estimate_cov(sequence, lib, k, e, nth, skmer_ver, ref_hist = None, theta = N
         theta = "NA" if (skmer_ver == "dipskmer") else None
         write_error_file(info_file, cov, g_len, eps, l, theta)
         return sample, cov, g_len, eps, l, theta
-    print(skmer_ver)
+    
     if skmer_ver == "dipskmer":
         (eps, lam, theta) = estimate_diploid_cov(sample, count, k, e, l, theta)
     elif skmer_ver == "reskmer":
@@ -65,7 +65,7 @@ def estimate_cov(sequence, lib, k, e, nth, skmer_ver, ref_hist = None, theta = N
     cov = (1.0 * l / (l - k)) * lam
     tot_seq = 1.0 * ksum * l / (l - k)
     g_len = int(tot_seq / cov)
-    print(cov, g_len, eps, lam)
+    
     if eps > error_rate_threshold or eps < 0:
         cov = "NA"
         g_len = "NA"
